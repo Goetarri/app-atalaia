bba28036bd03ccf7dd867d25ac30e839644dd100
Goetarri <Goetarri@users.noreply.github.com>
2025-11-19 23:14:13 +0100

    Text updates

--- es.js
+++ es.js
@@ -1,5 +1,5 @@
 const ES = {
-    "titulo_app": "Guía del Huésped",
+    "titulo_app": "Atalaia Terrace - Guia del Huésped",
     "bienvenida": "Bienvenido a tu hogar temporal",
     "selecciona": "Selecciona una opción:",
     "btn_electro": "Instrucciones Electrodomésticos",
@@ -14,16 +14,16 @@ const ES = {
     "sub_calefaccion": "Calefacción",
     "sub_otros": "Otros",
 
-    "lavadora_titulo": "Cómo usar:",
-    "lavadora_desc": "El detergente va en el compartimento izquierdo.",
+    "lavadora_titulo": "Cómo utilizar la lavadora:",
+    "lavadora_desc": "\n1. Vaciar los bolsillos.\n\n2. Colocar la ropa dentro del tambor.\n\n3. Añadir la pastilla de detergente dentro del tambor.\n\n4. Cerrar la puerta.\n\n5. Seleccionar el programa girando el selector. El programa más común es 'Sintéticos'.\n\n6. Seleccionar la temperatura y el tiempo. Al pulsar el botón 'Rápido/Perfecto' se reduce el tiempo de lavado.\n\n7. Pulsar el botón '⏯' para iniciar la lavadora.\n\n8. Una vez finalizado el lavado, girar el selector a la posición superior (parada).\n\n9. Abrir la puerta y sacar la ropa.",
     "lavadora_img": "images/wash_seq_es.png", 
 
-    "toldo_titulo": "Precaución:",
-    "toldo_desc": "Recoger si hay viento.",
+    "toldo_titulo": "Como funcionan los toldos:",
+    "toldo_desc": "Los toldos se controlan desde el mando que está a la izquierda de la puerta de la sala que da a la terraza. La terraza tiene tres toldos que se pueden controlar independientemente mediante los botones de la izquierda. Se puede activar uno o varios toldos simultáneamente. Cuando un toldo está seleccionado se ilumina el pulsador en color verde. Para desplegar o recoger los toldos pulsar los botones de arriba (replegar) o abajo (desplegar). El botón 'my' detiene el movimiento. Si se pulsa el botón 'my' estando el toldo parado, baja automáticamente hasta la barandilla.ecoger si hay viento.",
     "toldo_img": "images/awning_es.png",
 
-    "calefaccion_titulo": "Termostato:",
-    "calefaccion_desc": "Girar rueda derecha para subir temperatura.",
+    "calefaccion_titulo": "Como poner la calefacción:",
+    "calefaccion_desc": "La calefacción se controla desde el termostato ubicado en la sala, justo a la izquierda de la puerta del dormitorio. Tiene dos ruedas, la superior enciende o apaga la calefacción y la de abajo permite regular la temperatura. La temperatura recomendada es de 22ºC y viene indicada por el punto del regulador de la parte inferior.\n\nEn cuanto al mando superior, tiene tres posibles posiciones:\n\n - Izquierda (icono luna): calefacción APAGADA\n\n - Centro (icono sol): calefacción ENCENDIDA. La temperatura deseada se regula con la rueda inferior. Girar en sentido horario para aumentar la temperatura.\n\n - Derecha (icono reloj): sirve para programar la calefacción. Si deseáis programarla, decídnoslo y os ayudaremos encantados.",
     "calefaccion_img": "images/heating_es.png",
 
     "otros_titulo": "Varios:",
