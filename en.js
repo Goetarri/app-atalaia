805a35f9666796d9b62328fecd7fb353ea9eecbf
Goetarri <goetarri@gmail.com>
2025-11-20 18:17:14 +0100

    en.js update

--- en.js
+++ en.js
@@ -1,5 +1,5 @@
 const EN = {
-    "titulo_app": "Guest Guide",
+    "titulo_app": "Atalaia Terrace - Guest Guide",
     "bienvenida": "Welcome to your temporary home",
     "selecciona": "Select an option:",
     "btn_electro": "Appliance Instructions",
@@ -18,11 +18,11 @@ const EN = {
     "lavadora_desc": "\n1. Empty pockets.\n\n2.Place clother inside the drum.\n\n3. Add detergent pill inside the drum.\n\n4. Close the door\n\n5. Select the program by turning the knob. Most common program is 'Sinteticos (synthetics)'.\n\n6. Select temperature and time. Pressing the 'Speed/Perfect' button reduces washing time.\n\n7. Press '⏯' button to start the washer.\n\n8. Once washing is finished, turn the knob back to the top (stop) position.\n\n9. Open the door and take out the clothes.",
     "lavadora_img": "images/wash_seq_en.png", 
 
-    "toldo_titulo": "How to operate the awnings:",
+    "toldo_titulo": "How to control the awnings:",
     "toldo_desc": "The awnings are controlled from the keypad located to the left of the living room door that leads to the terrace. The terrace has three awnings, which can be controlled independently using the buttons on the left side of the remote.\n\nOne or more awnings can be activated simultaneously. When an awning is selected, its corresponding button illuminates green. To extend or retract the awnings, press the top up-arrow (retract) or the bottom down-arrow (extend). The 'my' button stops the movement. If the 'my' button is pressed while the awning is stationary, it will automatically move down to the railing level.tract if windy.",
     "toldo_img": "images/awning_en.png",
 
-    "calefaccion_titulo": "How to start the heating:",
+    "calefaccion_titulo": "How to turn the heating on:",
     "calefaccion_desc": "The heating is controlled from the thermostat located in the living room, just to the left of the bedroom door. It has two dials: the top one turns the heating on or off, and the bottom one allows you to adjust the temperature. The recommended temperature is 22ºC and is indicated by the dot on the lower knob.\n\nThe top dial has three possible positions:\n\n - Left (Moon Icon): Heating OFF.\n\n - Center (Sun Icon): Heating ON. The desired temperature is regulated using the lower dial. Turn wheel clockwise to increase temperature.\n\n - Right (Clock Icon): Used to program the heating. If you wish to program it, please let us know and we will be happy to help you.",
     "calefaccion_img": "images/heating_en.png",
 
