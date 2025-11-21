9681b20666c00da75632e77bfc7209802fd4fe58
Goetarri <goetarri@gmail.com>
2025-11-21 18:47:35 +0100

    icon updates

--- en.js
+++ en.js
@@ -12,23 +12,27 @@ const EN = {
     "sub_lavadora": "Washer",
     "sub_toldo": "Awning",
     "sub_calefaccion": "Heating",
+    "sub_persiana": "Blinds",
     "sub_otros": "Others",
 
     "lavadora_titulo": "How to use the washer:",
-    "lavadora_desc": "\n1. Empty pockets.\n\n2.Place clother inside the drum.\n\n3. Add detergent pill inside the drum.\n\n4. Close the door\n\n5. Select the program by turning the knob. Most common program is 'Sinteticos (synthetics)'.\n\n6. Select temperature and time. Pressing the 'Speed/Perfect' button reduces washing time.\n\n7. Press '⏯' button to start the washer.\n\n8. Once washing is finished, turn the knob back to the top (stop) position.\n\n9. Open the door and take out the clothes.",
-    "lavadora_img": "images/wash_seq_en.png", 
+    "lavadora_desc": "1. Empty pockets.\n\n2. Place clothes inside the drum.\n\n3. Add detergent pill inside the drum.\n\n4. Close the door.\n\n5. Select the program by turning the knob. Most common program is 'Sinteticos (synthetics)'.\n\n6. Select temperature and time. Pressing the 'Speed/Perfect' button reduces washing time.\n\n7. Press '⏯' button to start the washer.\n\n8. Once washing is finished, turn the knob back to the top (stop) position.\n\n9. Open the door and take out the clothes.",    "lavadora_img": "images/wash_seq_en.png", 
 
     "toldo_titulo": "How to control the awnings:",
-    "toldo_desc": "The awnings are controlled from the keypad located to the left of the living room door that leads to the terrace. The terrace has three awnings, which can be controlled independently using the buttons on the left side of the remote.\n\nOne or more awnings can be activated simultaneously. When an awning is selected, its corresponding button illuminates green. To extend or retract the awnings, press the top up-arrow (retract) or the bottom down-arrow (extend). The 'my' button stops the movement. If the 'my' button is pressed while the awning is stationary, it will automatically move down to the railing level.",
+    "toldo_desc": "The awnings are controlled from the keypad located to the left of the living room door that leads to the terrace. The terrace has three awnings, which can be controlled independently using the buttons on the left side of the remote.\n\nOne or more awnings can be activated simultaneously. When an awning is selected, its corresponding button illuminates green. \n\nTo extend or retract the awnings, press the top up-arrow (retract) or the bottom down-arrow (extend). The 'my' button stops the movement. If the 'my' button is pressed while the awning is stationary, it will automatically move down to the railing level.\n\nIn case of strong wind, there's nothing to worry about, the awings will retract automatically to avoid damage.",
     "toldo_img": "images/awning_en.png",
 
     "calefaccion_titulo": "How to turn the heating on:",
     "calefaccion_desc": "The heating is controlled from the thermostat located in the living room, just to the left of the bedroom door. It has two dials: the top one turns the heating on or off, and the bottom one controls the temperature. \n\nThe top dial has three possible positions:\n\n - Left (Moon Icon): Heating OFF.\n\n - Center (Sun Icon): Heating ON. The desired temperature is regulated using the lower dial. The comfort temperature (22ºC) is indicated with a dot on the lower knob.\n\n - Right (Clock Icon): Used to program the heating. If you wish to program it, please let us know and we will be happy to help you.",
     "calefaccion_img": "images/heating_en.png",
 
+    "persiana_titulo": "How do blinds work:",
+    "persiana_desc": "Blinds in the bedroom are operated using the strap located to the left of the door. To raise the blinds, pull the strap upwards. To lower them, unlock the strap by pulling the bottom part towards you, and then guide it upnwards. Do not pull directly upwards, as the blinds are locked to prevent slipping.",
+    "persiana_img": "images/persiana_en.png",
+
     "otros_titulo": "Misc:",
     "otros_desc": "Hairdryer in bathroom.",
-    "otros_img": "images/otros_en.jpg",
+    "otros_img": "images/otros_en.png",
 
     "titulo_restaurantes": "Eat & Drink",
     "rest_1_nombre": "Bar La Plaza:",
--- es.js
+++ es.js
@@ -12,23 +12,28 @@ const ES = {
     "sub_lavadora": "Lavadora",
     "sub_toldo": "Toldo",
     "sub_calefaccion": "Calefacción",
+    "sub_persiana": "Persiana",
     "sub_otros": "Otros",
 
     "lavadora_titulo": "Cómo utilizar la lavadora:",
-    "lavadora_desc": "\n1. Vaciar los bolsillos.\n\n2. Colocar la ropa dentro del tambor.\n\n3. Añadir la pastilla de detergente dentro del tambor.\n\n4. Cerrar la puerta.\n\n5. Seleccionar el programa girando el selector. El programa más común es 'Sintéticos'.\n\n6. Seleccionar la temperatura y el tiempo. Al pulsar el botón 'Rápido/Perfecto' se reduce el tiempo de lavado.\n\n7. Pulsar el botón '⏯' para iniciar la lavadora.\n\n8. Una vez finalizado el lavado, girar el selector a la posición superior (parada).\n\n9. Abrir la puerta y sacar la ropa.",
+    "lavadora_desc": "1. Vaciar los bolsillos.\n\n2. Colocar la ropa dentro del tambor.\n\n3. Añadir la pastilla de detergente dentro del tambor.\n\n4. Cerrar la puerta.\n\n5. Seleccionar el programa girando el selector. El programa más común es 'Sintéticos'.\n\n6. Seleccionar la temperatura y el tiempo. Al pulsar el botón 'Rápido/Perfecto' se reduce el tiempo de lavado.\n\n7. Pulsar el botón '⏯' para iniciar la lavadora.\n\n8. Una vez finalizado el lavado, girar el selector a la posición superior (parada).\n\n9. Abrir la puerta y sacar la ropa.",
     "lavadora_img": "images/wash_seq_es.png", 
 
     "toldo_titulo": "Como funcionan los toldos:",
-    "toldo_desc": "Los toldos se controlan desde el mando que está a la izquierda de la puerta de la sala que da a la terraza. La terraza tiene tres toldos que se pueden controlar independientemente mediante los botones de la izquierda. Se puede activar uno o varios toldos simultáneamente. Cuando un toldo está seleccionado se ilumina el pulsador en color verde. Para desplegar o recoger los toldos pulsar los botones de arriba (replegar) o abajo (desplegar). El botón 'my' detiene el movimiento. Si se pulsa el botón 'my' estando el toldo parado, baja automáticamente hasta la barandilla.ecoger si hay viento.",
+    "toldo_desc": "Los toldos se controlan desde el mando que está a la izquierda de la puerta de la sala que da a la terraza. La terraza tiene tres toldos que se pueden controlar independientemente mediante los botones de la izquierda. Se puede activar uno o varios toldos simultáneamente. Cuando un toldo está seleccionado se ilumina el pulsador en color verde. \n\nPara desplegar o recoger los toldos pulsar los botones de arriba (replegar) o abajo (desplegar). El botón 'my' detiene el movimiento. Si se pulsa el botón 'my' estando el toldo parado, baja automáticamente hasta la barandilla.\n\nSi se levanta viento fuerte, no hay que preocuparse, los toldos se recogerán automáticamente para evitar daños.",
     "toldo_img": "images/awning_es.png",
 
     "calefaccion_titulo": "Como poner la calefacción:",
     "calefaccion_desc": "La calefacción se controla desde el termostato ubicado en la sala, justo a la izquierda de la puerta del dormitorio. Tiene dos ruedas, la superior enciende o apaga la calefacción y la de abajo permite regular la temperatura.\n\nEn cuanto al mando superior, tiene tres posibles posiciones:\n\n - Izquierda (icono luna): calefacción APAGADA\n\n - Centro (icono sol): calefacción ENCENDIDA. La temperatura deseada se regula con la rueda inferior. La temperatura recomendada es de 22ºC y viene indicada por el punto del regulador de la parte inferior.\n\n - Derecha (icono reloj): sirve para programar la calefacción. Si deseáis programarla, decídnoslo y os ayudaremos encantados.",
     "calefaccion_img": "images/heating_es.png",
 
+    "persiana_titulo": "Como funciona la persiana:",
+    "persiana_desc": "La persiana de la habitación se maneja con la correa ubicada a la izquierda de la puerta. Para levantarla, tirar de la correa hacia arriba. Para bajarla, desbloquear la correa tirado de la parte de abajo hacia uno mismo, y acompañarla hacia arriba. No tirar directamente hacia arriba, porque la persiana está bloqueada para evitar que deslice.",
+    "persiana_img": "images/persiana_es.png",
+
     "otros_titulo": "Varios:",
     "otros_desc": "Secador en el baño.",
-    "otros_img": "images/otros_es.jpg",
+    "otros_img": "images/otros_es.png",
 
     "titulo_restaurantes": "Comer y Beber",
     "rest_1_nombre": "Bar La Plaza:",
