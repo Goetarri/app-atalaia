434260190c84dd081fdd4ca45fada8598b14b6c3
Goetarri <goetarri@gmail.com>
2025-11-29 16:04:43 +0100

    fixes

--- es.js
+++ es.js
@@ -32,7 +32,7 @@ const ES = {
 
     // Mantener el resto de tu contenido existente...
     "lavadora_titulo": "Cómo utilizar la lavadora:",
-    "lavadora_desc": "1. Vaciar los bolsillos.\n2. Colocar la ropa dentro del tambor.\n3. Añadir la pastilla de detergente dentro del tambor.\n4. Cerrar la puerta.\n5. Seleccionar el programa girando el selector. El programa más común es 'Sintéticos'.\n6. Seleccionar la temperatura y el tiempo. Al pulsar el botón 'Rápido/Perfecto' se reduce el tiempo de lavado.\n7. Pulsar el botón '⏯' para iniciar la lavadora.\n8. Una vez finalizado el lavado, girar el selector a la posición superior (parada).\n9. Abrir la puerta y sacar la ropa.",
+    "lavadora_desc": "1. Vaciar los bolsillos.\n2. Colocar la ropa dentro del tambor.\n3. Añadir la pastilla de detergente dentro del tambor.\n4. Cerrar la puerta.\n5. Seleccionar el programa girando el selector. El programa más común es 'Sintéticos'.\n6. Seleccionar la temperatura y el tiempo. Al pulsar el botón 'Rápido/Perfecto' se reduce el tiempo de lavado.\n7. Pulsar el botón ⏯ para iniciar la lavadora.\n8. Una vez finalizado el lavado, girar el selector a la posición superior (parada).\n9. Abrir la puerta y sacar la ropa.",
     "lavadora_img": [
         "images/wash_01_es.png", "images/wash_02_es.png", "images/wash_03_es.png",
         "images/wash_04_es.png", "images/wash_05_es.png", "images/wash_06_es.png",
